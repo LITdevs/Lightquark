5cb7fa3b006baf1f29ee7629f63ccb7762608d94
Amy <amy@litdevs.org>
2023-04-02 09:34:33 +0000

    Client attribute search
    #LIGHTQUARK-18 State Done

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -3,9 +3,40 @@ import path from "path";
 
 const router: Router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
-});
+router.get("/", (req: Request, res: Response) => {
+	res.sendFile("public/index.html", {root: path.resolve()});
+})
+
+router.get("/developers/interoperability", (req: Request, res: Response) => {
+	res.sendFile("public/interop.html", {root: path.resolve()});
+})
+
+router.get("/developers/interoperability/clientAttributeSearch", (req: Request, res: Response) => {
+	res.sendFile("public/clientAttributeSearch.html", {root: path.resolve()});
+})
+
+router.get("/developers/interoperability/api/attributes", async (req: Request, res: Response) => {
+	const Messages = db.getMessages();
+
+	// Perform black magic to find all keys in clientAttributes
+
+	let keys = await Messages.aggregate([
+		{ $unwind: "$specialAttributes" },
+		{ $match: { "specialAttributes.type": "clientAttributes" } },
+		{ $project: { _id: 0, keys: { $objectToArray: "$specialAttributes" } } },
+		{ $unwind: "$keys" },
+		{ $match: { "keys.k": { $ne: "type" } } },
+		{
+			$group: {
+				_id: "$keys.k",
+				count: { $sum: 1 }
+			}
+		},
+		{ $project: { _id: 0, key: "$_id", count: 1 } }
+	]);
+
+	res.json(keys);
+})
 
 router.get("/v1/ping", (req : Request, res : Response) => {
 	res.contentType("text/plain");
@@ -26,6 +57,7 @@ import fs from "fs";
 const knownClients = JSON.parse(fs.readFileSync("src/util/knownClients.json").toString());
 import CapabilityParser from "../util/CapabilityParser.js";
 import ServerErrorReply from "../classes/reply/ServerErrorReply.js";
+import db from "../db.js";
 
 router.get("/features/:clientName", async (req: Request, res: Response) => {
 	try {
