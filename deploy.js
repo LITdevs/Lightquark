e87edf12b100cdcd9688ba1beb0a5fc5607dab65
BrisanceBot <88146055+BrisanceBot@users.noreply.github.com>
2023-04-14 18:38:53 +0000

    * Confusion *

--- deploy.js
+++ deploy.js
@@ -1,10 +1,11 @@
 let token = process.env.SPACE_TOKEN;
 let environment = process.env.JB_SPACE_GIT_BRANCH === "dev" ? "lightquark-dev" : "lightquark";
+console.log("Branch: ", process.env.JB_SPACE_GIT_BRANCH);
 import axios from "axios";
-import {exec} from "child_process";
+/*import {exec} from "child_process";
 
 // Get git branch
-/*exec("git rev-parse --abbrev-ref HEAD", (error, stdout, stderr) => {
+exec("git rev-parse --abbrev-ref HEAD", (error, stdout, stderr) => {
 	environment = stdout.trim();
 	console.log(`Environment: ${environment}`);
 	environment = environment === "dev" ? "lightquark-dev" : "lightquark"*/
