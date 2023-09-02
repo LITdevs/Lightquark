ba5082400a165353c50e50e0e55087da7e98be7a
Amy <amy@litdevs.org>
2023-09-02 09:54:44 +0000

    Re-order ConstantID.ts

--- src/util/ConstantID.ts
+++ src/util/ConstantID.ts
@@ -1,8 +1,15 @@
 import {Types} from "mongoose";
 
-export const ConstantID_DMvQuark = new Types.ObjectId("000000000000000000000000")
+// Users
 export const ConstantID_SystemUser = new Types.ObjectId("000000000000000000000001")
+
+// vQuarks
+export const ConstantID_DMvQuark = new Types.ObjectId("000000000000000000000000")
+
+// Roles
 export const ConstantID_OwnerRole = new Types.ObjectId("000000000000000000000002")
-export const ConstantID_OwnerAssignment = new Types.ObjectId("000000000000000000000003")
 export const ConstantID_DenyRole = new Types.ObjectId("000000000000000000000004")
+
+// Permission assignments
 export const ConstantID_DenyAssignment = new Types.ObjectId("000000000000000000000005")
+export const ConstantID_OwnerAssignment = new Types.ObjectId("000000000000000000000003")
